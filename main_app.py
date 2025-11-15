25a0f422408b4084c2b0a2ecbf365e50711566d4
Joy Wanjiru Gaitho <cirujoygaitho7103@gmail.com>
2025-11-15 18:03:35 +0300

    used import to link it it my_helpers.py

---
+++ main_app.py
@@ -0,0 +1,7 @@
+# main_app.py content
+import my_helpers
+# use the functions from the module
+message = my_helpers.greet("isabela")
+result = my_helpers.calculate_square(7)
+print(message)
+print(f"7 squared is: {result}")
