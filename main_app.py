053458aeebc2d06f665116f76def33b64b471752
Joy Wanjiru Gaitho <cirujoygaitho7103@gmail.com>
2025-11-15 18:33:22 +0300

    added valuable info

--- main_app.py
+++ main_app.py
@@ -1,7 +1,7 @@
 # main_app.py content
-import my_helpers # import statements links my_helpers.py and main_app.py modules
+import my_helpers # import statements help us access functions defined in my_helpers.py module
 # use the functions from the module
-message = my_helpers.greet("isabela")
+message = my_helpers.greet("isabela") # this is a standard way in python to call a function that has been imported from a module
 result = my_helpers.calculate_square(7)
 print(message)
 print(f"7 squared is: {result}")
