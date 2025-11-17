bc2e2da0f31bafce6ea3c1b4c41616357a297e51
Joy Wanjiru Gaitho <cirujoygaitho7103@gmail.com>
2025-11-17 18:29:50 +0300

    my favourite book

---
+++ dictionaries.py
@@ -0,0 +1,3 @@
+fav_book = {'title': 'dune', 'author': 'timothee chamelet', 'genre': 'science fiction', 'publication year': 1965}
+book_genre = fav_book.get('genre')
+print(f"the genre of the book is: {book_genre}")
